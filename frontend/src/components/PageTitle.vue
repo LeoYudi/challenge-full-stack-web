1ae5c176f82398907eeaeaebccf192d6765629ed
Leonardo Higuti <lyhiguti@gmail.com>
2025-02-11 18:18:01 -0300

    feat: add page title component

---
+++ frontend/src/components/PageTitle.vue
@@ -0,0 +1,14 @@
+<template>
+  <div
+    class="d-flex justify-center bg-grey-lighten-2 pa-2 mb-5"
+    style="height: 44px"
+  >
+    {{ title }}
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { defineProps } from "vue";
+
+defineProps<{ title: string }>();
+</script>
