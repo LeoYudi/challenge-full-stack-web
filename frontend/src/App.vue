f96f676f6b54f462da1d831b3c898d4432a18389
Leonardo Higuti <lyhiguti@gmail.com>
2025-02-11 16:52:24 -0300

    feat: add public and protected layouts

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,19 @@
 <template>
   <v-app>
-    <router-view />
+    <component :is="Layout">
+      <router-view />
+    </component>
   </v-app>
 </template>
 
 <script lang="ts" setup>
-  //
+import { useRoute } from "vue-router";
+import PublicLayout from "@/layouts/public.vue";
+import ProtectedLayout from "@/layouts/protected.vue";
+
+const route = useRoute();
+
+const Layout = computed(() =>
+  route.name?.includes("login") ? PublicLayout : ProtectedLayout
+);
 </script>
---
+++ frontend/src/layouts/protected.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-snackbar v-model="snackbarStore.show">
+    {{ snackbarStore.textAlert }}
+
+    <template #actions>
+      <v-btn icon="mdi-close-thick" @click="snackbarStore.closeMessage" />
+    </template>
+  </v-snackbar>
+  <v-main>
+    <v-app-bar>
+      <div class="pa-3 w-100 h-100">
+        <v-img :src="Logo" />
+      </div>
+    </v-app-bar>
+
+    <v-navigation-drawer style="box-sizing: border-box">
+      <v-list-item class="bg-grey-darken-2">
+        <v-list-item-title class="text-h5">Módulo Acadêmico</v-list-item-title>
+      </v-list-item>
+      <v-list-item
+        to="/students"
+        link
+        active-class="bg-grey-lighten-2"
+        title="Alunos"
+      />
+      <v-list-item link title="Usuários" />
+    </v-navigation-drawer>
+
+    <div class="pa-3">
+      <router-view />
+    </div>
+  </v-main>
+</template>
+
+<script lang="ts" setup>
+import Logo from "@/assets/grupo-a.png";
+import { useSnackbarStore } from "@/stores/snackbar";
+
+const snackbarStore = useSnackbarStore();
+</script>
